46af217148dae5f40b2cbd1132421946b3e45ab6
basembaaem8 <69736605+basembaaem8@users.noreply.github.com>
2020-08-16 00:53:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#    joker ^_*
+#    joker Maser ♡_♡
 
 # hala ya rohy hhhhhhh
 #    colors
@@ -10,7 +10,7 @@ cyan='\e[1;36m'
 white='\e[1;37m'
 yellow='\e[1;33m'
 b='\033[90;1m'
-termux-open https://www.youtube.com/channel/UCFCxf5tyoKpQyvsq-ubOsxg
+termux-open https://m.youtube.com/channel/UClD6P39ETTOCZKU6W64kjrA
 #   	joker hhhhhhh
 clear
 pkg install figlet -y
@@ -33,14 +33,15 @@ echo -e "$green"
 figlet -f mono12 "Install"
 echo -e "$yellow           Please Wait  "
 termux-setup-storage
-mkdir /sdcard/JOKER-EGYPT
+mkdir /sdcard/Joker Maser
 cd $HOME/
-git clone https://github.com/Joker-Egy/jokerr
-cd jokerr
-mv Joker-Egy.zip $HOME/
+git clone https://github.com/basem/joker-maser
+cd joker
+mv Joker-maser.zip $HOME/
 cd $HOME/
-unzip Joker-Egy.zip
-rm -rif Joker-Egy.zip
-rm -rif jokerr
-cd Joker-Egy
-python joker.py
+unzip Joker-maser.zip
+rm -rif Joker-maser.zip
+rm -rif basem
+cd Joker-maser
+python basem.py
+
